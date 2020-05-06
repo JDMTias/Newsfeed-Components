53a63281f7bf67c139fbb3e5a3066eb1f310c144
JDMTias <matiasiturbide@gmail.com>
2020-05-06 17:17:29 -0600

    all steps done except not working trying to debug

--- Menu/Menu.js
+++ Menu/Menu.js
@@ -21,11 +21,11 @@ let menuItems = [
 
   // The function takes an array as its only argument.
 
-  const menuMaker = (menuData) => {
+const menuMaker = (menuData) => {
 
-    const menuContainer = document.createElement('div');
-    const menuList = document.createElement('ul')
-  }
+  const menuContainer = document.createElement('div');  
+  const menuList = document.createElement('ul')
+
  
   // Step 2: Inside this function, iterate over the array creating a list item <li> element for each item in the array. 
   // Add those items to the <ul> 
@@ -38,21 +38,22 @@ let menuItems = [
     
   });
 
-
-
- 
-
   // Step 3: Using a DOM selector, select the menu button (the element with a class of 'menu-button') currently on the DOM.
 
   const menuButton = document.querySelector('.menu-button');
 
   // Step 4: add a click event listener to the menu button. When clicked it should toggle the class 'menu--open' on the menu (your div with a 'menu' class).
+  
   menuButton.addEventListener('click', event => {
-    panelOpenButton.classList.toggle('hide-btn')
-    panelCloseButton.classList.toggle('hide-btn')
-    panelContent.classList.toggle('toggle-on')
-  })
+    menuButton.classList.toggle('menu--open')
+  });
 
   // Step 5: return the menu component.
 
+  return menuContainer;
+
+  }
+
   // Step 6: add the menu component to the DOM.
+
+  headers.appendChild(menuContainer)
